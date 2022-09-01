d81c10f8d421606def971181c0c4f68cdf6a132c
Rifqi Naufal Abdjul <rifqi.abdjul23@gmail.com>
2022-09-01 18:59:38 +0700

    implement nextauth

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,10 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
+    NEXTAUTH_SECRET: string;
     JWT_SECRET: string;
     DATABSE_URL: string;
     BCRYPT_SALT_ROUNDS: string;
+    GOOGLE_CLIENT_EMAIL: string;
+    GOOGLE_PRIVATE_KEY: string;
   }
 }
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,62 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import prisma from '@services/prisma';
+import { NextApiRequest, NextApiResponse } from 'next';
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import bcrypt from 'bcrypt';
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, {
+    providers: [
+      CredentialsProvider({
+        name: 'credentials',
+        credentials: {
+          nim: {
+            label: 'NIM',
+            placeholder: 'NIM',
+            required: true,
+          },
+          password: {
+            label: 'Password',
+            placeholder: 'Password',
+            required: true,
+          },
+        },
+        async authorize(credentials) {
+          if (!credentials || !credentials.nim || !credentials.password) return null;
+          const user = await prisma.user.findUnique({
+            where: {
+              nim: credentials.nim,
+            },
+            select: {
+              id: true,
+              nim: true,
+              name: true,
+              email: true,
+              password: true,
+            },
+          });
+          if (user && bcrypt.compareSync(credentials.password, user.password)) {
+            return user;
+          }
+          return null;
+        },
+      }),
+    ],
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+      jwt: async ({ token, user }) => {
+        // jwt payload
+        if (user) {
+          token.user = user;
+        }
+        return token;
+      },
+      session: async ({ session, token }: any) => {
+        // session callback, add id and role properties
+        session.user = token.user;
+        return session;
+      },
+    },
+  });
+}
