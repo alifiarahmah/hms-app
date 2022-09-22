5a14efa13f25c07a360d76106cdaa3d9a891b1a2
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-09-22 18:48:55 +0700

    Add modal on gallery

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,33 +1,80 @@
-import { Button, Heading, Image, Select, SimpleGrid, Stack, Text } from '@chakra-ui/react';
+import {
+  Button,
+  Heading,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Select,
+  SimpleGrid,
+  Stack,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 import Layout from 'components/layout';
 
 // TODO: implement pagination
 
 const Gallery = () => {
-  return (
-    <Layout>
-      <Heading as="h1" mb={10}>
-        Gallery
-      </Heading>
-      <Select bg="" fontSize="1.25rem">
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
-      </Select>
-      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} mx={20} my={10}>
-        {Array.from({ length: 15 }).map((_, i) => (
-          <Image key={i} alt="random" src="https://source.unsplash.com/random" />
-        ))}
-      </SimpleGrid>
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-      <Stack direction="row" justifyContent="center" alignItems="center">
-        <Button isDisabled>First</Button>
-        <Button isDisabled>Prev</Button>
-        <Text px={5}>1/10</Text>
-        <Button>Next</Button>
-        <Button>Last</Button>
-      </Stack>
-    </Layout>
+  return (
+    <>
+      <Layout>
+        <Heading as="h1" mb={10}>
+          Gallery
+        </Heading>
+        <Select bg="" fontSize="1.25rem">
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={10} mx={20} my={10}>
+          {Array.from({ length: 15 }).map((_, i) => (
+            <Image
+              cursor="pointer"
+              onClick={onOpen}
+              key={i}
+              alt="random"
+              src="https://source.unsplash.com/random"
+            />
+          ))}
+        </SimpleGrid>
+
+        <Stack direction="row" justifyContent="center" alignItems="center">
+          <Button isDisabled>First</Button>
+          <Button isDisabled>Prev</Button>
+          <Text px={5}>1/10</Text>
+          <Button>Next</Button>
+          <Button>Last</Button>
+        </Stack>
+      </Layout>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Random Image</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Image alt="random" src="https://source.unsplash.com/random" mb={5} />
+            <Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque eget nulla sit amet
+              quam lacinia hendrerit
+            </Text>
+          </ModalBody>
+
+          <ModalFooter>
+            {/* <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Download
+            </Button> */}
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
