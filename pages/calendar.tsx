177faeda4793230709ce45fc6763b106ccc602d1
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-09-22 19:58:31 +0700

    Add calendar

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,7 +1,15 @@
+import { Heading } from '@chakra-ui/react';
 import Layout from 'components/layout';
+import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
 
 const Index = () => {
-  return <Layout>Welcome to HMSApp Calendar!</Layout>;
+  return (
+    <Layout>
+      <Calendar />
+      <Heading mt={5}>Upcoming events</Heading>
+    </Layout>
+  );
 };
 
 export default Index;
