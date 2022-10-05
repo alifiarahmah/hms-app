145edcb14682e277f38315a36e9f04c2f46e18e8
Alifia Rahmah <alifiarahmah03@gmail.com>
2022-10-06 02:32:58 +0700

    kalender publik

--- pages/calendar.tsx
+++ pages/calendar.tsx
@@ -1,41 +1,120 @@
-import { Box, Button, Heading, HStack, Stack, Text } from '@chakra-ui/react';
+import {
+  Button,
+  Heading,
+  InputGroup,
+  Modal,
+  ModalContent,
+  ModalFooter,
+  ModalOverlay,
+  Stack,
+  Text,
+  useDisclosure,
+  useToast,
+} from '@chakra-ui/react';
 import Layout from 'components/layout';
 import Link from 'components/link';
 import moment from 'moment';
+import { useEffect, useState } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const localizer = momentLocalizer(moment);
+type Event = {
+  uid?: string;
+  title: string;
+  description?: string;
+  meetingLink?: string;
+  start: Date;
+  end: Date;
+  rrule?: string;
+};
 
-const Index = () => {
+const blankEvent = {
+  start: new Date(),
+  end: new Date(),
+  title: '',
+  description: '',
+  linkMeeting: '',
+};
+
+const Event = () => {
+  const localizer = momentLocalizer(moment);
+  const [date, setDate] = useState(new Date());
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const [event, setEvent] = useState<Event>(blankEvent);
+  const [events, setEvents] = useState<Event[]>([]);
+  const toast = useToast();
+
+  const handleSelect = (event: Event) => {
+    setEvent(event);
+    onOpen();
+  };
+
+  useEffect(() => {
+    fetch('/api/admin/calendar', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        const { data } = res;
+
+        setEvents(
+          data.map((event: any) => {
+            event.start = new Date(event.start);
+            event.end = new Date(event.end);
+            return event;
+          })
+        );
+      })
+      .catch((err) => {
+        toast({
+          title: 'Error',
+          description: err.message,
+          status: 'error',
+        });
+      });
+  }, [toast]);
+
   return (
-    <Layout>
-      <Calendar
-        localizer={localizer}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
-      <Heading pt={5}>Upcoming events</Heading>
-      <Stack py={5}>
-        {Array.from({ length: 3 }).map((_, i) => {
-          return (
-            <Box key={i}>
-              <Text fontWeight="bold">Event {i}</Text>
-              <HStack justifyContent="space-between">
-                <Text>DD-MM-YY HH:MM</Text>
-                <Text>Medkominfo</Text>
-              </HStack>
-              <Text>Lorem ipsum dolor sit amet</Text>
-              <Link href="https://google.com" isExternal>
-                <Button>Join Meeting</Button>
-              </Link>
-            </Box>
-          );
-        })}
+    <Layout title="Kalender">
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent py={6} px={4} gap={2}>
+          <Heading size="xs">Title</Heading>
+          <InputGroup>
+            <Text>{event.title}</Text>
+          </InputGroup>
+          <Heading size="xs">Description</Heading>
+          <InputGroup>
+            <Text>{event.description}</Text>
+          </InputGroup>
+          <Heading size="xs">Start</Heading>
+          <Text>{moment(new Date(event.start)).format('LLL')}</Text>
+          <Heading size="xs">End</Heading>
+          <Text>{moment(new Date(event.end)).format('LLL')}</Text>
+          <Heading size="xs">Link Meeting</Heading>
+          <Link href={event.meetingLink} isExternal>
+            <Button>Join Meeting</Button>
+          </Link>
+
+          <ModalFooter>
+            <Button colorScheme="primary" mr={3} onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Stack w="100%">
+        <Calendar
+          onNavigate={(date) => setDate(date)}
+          selectable
+          date={moment(date).startOf('month').toDate()}
+          views={['month']}
+          localizer={localizer}
+          events={events}
+          onSelectEvent={handleSelect}
+        />
       </Stack>
     </Layout>
   );
 };
 
-export default Index;
+export default Event;
